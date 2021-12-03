b585f8c3bd52b9666088c13840773961dca0e187
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-12-03 15:22:29 +0100

    Add input with file type

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('express-handlebars');
 const path = require('path');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 const app = express();
 app.engine('hbs', hbs());
@@ -38,15 +40,20 @@ app.get('/hello/:name', (req, res) => {
   res.render('hello', { layout: false, name: req.params.name });
 });
 
-app.post('/contact/send-message', (req, res) => {
-  const { author, sender, title, message } = req.body;
+app.post(
+  '/contact/send-message',
+  upload.single('project'),
+  (req, res, next) => {
+    const { author, sender, title, message } = req.body;
+    const project = req.file;
 
-  if (author && sender && title && message) {
-    res.render('contact', { isSent: true });
-  } else {
-    res.render('contact', { isError: true });
+    if (author && sender && title && message && project) {
+      res.render('contact', { isSent: true, fileName: project.originalname });
+    } else {
+      res.render('contact', { isError: true });
+    }
   }
-});
+);
 
 app.use((req, res) => {
   res.status(404).render('error404');
